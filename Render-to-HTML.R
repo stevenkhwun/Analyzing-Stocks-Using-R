df3440c4d7b25170300254f06a9463de51dac3d6
stevenkhwun <stevenkhwun@gmail.com>
2024-08-18 15:05:04 +0800

    Some files added.

--- Render-to-HTML.R
+++ Render-to-HTML.R
@@ -1,3 +1,5 @@
 # Create HTML document in the sub-directory 'HTML'
 library(rmarkdown)
 render("Analyzing-Stocks-Using-R.Rmd", "html_document", output_dir = "HTML")
+render("Multiple_Chart_Series.Rmd", "html_document", output_dir = "HTML")
+render("quantmod_Notes.Rmd", "html_document", output_dir = "HTML")
